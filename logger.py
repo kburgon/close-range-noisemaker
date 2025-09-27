0595a9a688cacc39f0c7e316ec63c0e6ab210fab
Kevin Burgon <buburgo@gmail.com>
2025-09-27 07:44:07 -0600

    Add logger, initial event implementation

---
+++ logger.py
@@ -0,0 +1,34 @@
+import sqlite3
+from datetime import datetime
+
+def ensureCreated(conn: sqlite3.Connection) -> None:
+    creation_query = """
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        event TEXT,
+        distance INTEGER
+    );
+    """
+
+    cursor = conn.cursor()
+    cursor.execute(creation_query)
+    conn.commit()
+
+def log_event(event: str, distance: int, dbName: str = ":memory:") -> None:
+    try:
+        with sqlite3.connect(dbName) as conn:
+            insertQuery = '''
+                INSERT INTO events (timestamp, event, distance)
+                VALUES (?, ?, ?)
+            '''
+            timestamp = datetime.now().isoformat(timespec='milliseconds')
+            params = (timestamp, event, distance)
+
+            ensureCreated(conn)
+            cur = conn.cursor()
+            cur.execute(insertQuery, params)
+            conn.commit()
+
+    except sqlite3.Error as ex:
+        print(ex)
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import logger
+import distance_listener
+from sound_playback_helper import sound_player
 # Steps
 
-# Create sensor module
+# Create sensor module - done
 
-# Establish distance listener
+# Establish distance listener - done
 
-# Give distance listener events for entered range, within range, and left range events
+# Give distance listener events for entered range, within range, and left range events - done
 
-# Create event logger
+# Create event logger - done
 
 # Create event methods to call
 
 # Within event methods, create event logging, sound actions
+dbName = 'events.db'
+
+def onEnterActiveRange(distance):
+    logger.log_event('enter_range', distance, dbName)
+    player = sound_player.SoundPlayer()
+    player.play_rotated_sound()
 
 
 if (__name__ == "__main__"):
