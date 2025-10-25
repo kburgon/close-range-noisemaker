0a2ca7b4bd0205cc4f3bf82b64b0a0ed83fda2b0
Kevin Burgon <buburgo@gmail.com>
2025-10-24 23:00:27 -0600

    Add events to class with shared SoundPlayer

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from sound_playback_helper.sound_player import SoundPlayer
 
 dbName = 'events.db'
 
-def onEnterActiveRange(distance: int) -> None:
-    logger.log_event('enter_range', distance, dbName)
-    player = SoundPlayer()
-    player.all_sounds = player.list_sounds()
-    player.play_rotated_sound()
 
-def onWithinActiveRange(distance: int) -> None:
-    logger.log_event('within_range', distance, dbName)
-    player = SoundPlayer()
-    player.all_sounds = player.list_sounds()
-    player.play_rotated_sound()
+class DistanceEvents:
+    def __init__(self):
+        self.sound_player = SoundPlayer()
 
-def onLeaveActiveRange(distance: int) -> None:
-    logger.log_event('leave_range', distance, dbName)
+    def onEnterActiveRange(self, distance: int) -> None:
+        logger.log_event('enter_range', distance, dbName)
+        self.sound_player.play_rotated_sound()
 
-def onError(exception: Exception) -> None:
-    logger.log_event(f"error: {exception}", 0, dbName)
+    def onWithinActiveRange(self, distance: int) -> None:
+        logger.log_event('within_range', distance, dbName)
+        self.sound_player.play_rotated_sound()
+
+    def onLeaveActiveRange(self, distance: int) -> None:
+        logger.log_event('leave_range', distance, dbName)
+
+    def onError(self, exception: Exception) -> None:
+        logger.log_event(f"error: {exception}", 0, dbName)
+
 
 if (__name__ == "__main__"):
     print('Started')
@@ -29,8 +31,9 @@ if (__name__ == "__main__"):
     active_distance_sec = 100
     check_frequency_sec = 0.25
     retrigger_frequency_sec = 10
+    de = DistanceEvents()
     listener = DistanceListener(trigger_pin, echo_pin, active_distance_sec, check_frequency_sec, retrigger_frequency_sec)
     try:
-        listener.listen(onEnterActiveRange, onWithinActiveRange, onLeaveActiveRange, onError)
+        listener.listen(de.onEnterActiveRange, de.onWithinActiveRange, de.onLeaveActiveRange, de.onError)
     finally:
         listener.close()
